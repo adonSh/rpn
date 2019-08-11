e2566eced50045be17f62f3678c9e80d83b41938
Adon Shapiro <adon.shapiro@gmail.com>
2019-08-11 18:35:38 -0500

    initial commit; rpn integer calculator

---
+++ rpn.py
@@ -0,0 +1,61 @@
+#TODO: floats, better logic
+import sys
+from typing import List, Callable, Optional
+from enum import Enum, auto
+
+Stack = List[int]
+
+def empty(s: Stack) -> str:
+    s.clear()
+    return ''
+
+def pop(s: Stack) -> int:
+    return s.pop()
+
+def push(s: Stack, n: int) -> int:
+    s.append(n)
+    return n
+
+def is_op(sym: str) -> bool:
+    return (sym == '+' or 
+            sym == '-' or
+            sym == '*' or 
+            sym == '/' or
+            sym == 'c' or
+            sym == 'q')
+
+def is_valid(sym: str) -> bool:
+    return (sym == 'q' or
+            sym == 'c' or
+            is_op(sym) or
+            sym.isdigit())
+
+def op(sym: str) -> Callable[[int, int], int]:
+    if sym == '+':
+        return lambda x, y: y + x
+    if sym == '-':
+        return lambda x, y: y - x
+    if sym == '*':
+        return lambda x, y: y * x
+#   if sym == '/':
+    return lambda x, y: y // x
+
+def eval(s: Stack, op: Callable[[int, int], int]) -> int:
+    return push(s, op(s.pop(), s.pop()))
+    
+def loop(s: Stack, cur: str) -> str:
+    if cur == 'c':
+        return empty(s)
+    if cur == 'q':
+        sys.exit()
+    if not is_valid(cur):
+        return 'invalid input'
+    if is_op(cur):
+        return str(eval(s, op(cur))) if len(s) > 1 else 'too few args'
+    return str(push(s, int(cur)))
+
+# main
+stack: Stack = []
+
+while True:
+    print(loop(stack, input('> ')))
