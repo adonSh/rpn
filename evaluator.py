254c01156a6ae26eca8dae4666ae442ce39b6081
Adon Shapiro <adon.shapiro@gmail.com>
2019-08-12 19:38:03 -0500

    clarified evaluator

--- evaluator.py
+++ evaluator.py
@@ -5,9 +5,4 @@ Op    = Callable[[int, int], int]
 Exp   = Union[int, Op]
 
 def evaluate(s: Stack[int], e: Exp) -> int:
-    val: int
-    if isinstance(e, int):
-        val = push(s, e)
-    else:
-        val = e(pop(s), pop(s))
-    return val
+    return push(s, e if isinstance(e, int) else e(pop(s), pop(s)))
